f27ffb9f48dc4a3ba0aaf54af9df034a89156be2
Yash Sharma <ysharma3@ucmerced.edu>
2019-10-18 00:08:14 -0700

    Setup basic signin template

--- BackEnd/signIn.js
+++ BackEnd/signIn.js
@@ -1,67 +1,48 @@
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+
+var email="someone@example.com";
+var password = "password";
+
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error){
+  //Error handling
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorCode);
+  console.log(errorMessage);
   
+});
 
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      {
-        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        requireDisplayName: True
-      }
-    ]
-  });
-
-
-
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+  //Error handling
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorCode);
+  console.log(errorMessage);
   
-  var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-      }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
-  };
-  
-  // Initialize Firebase with a default Firebase project
-firebase.initializeApp(firebaseConfig);
+});
 
-// Initialize Firebase with a second Firebase project
-var otherProject = firebase.initializeApp(otherProjectFirebaseConfig, "other");
+firebase.auth().signOut().then(function() {
+  console.log("Sign-out successful");
+}).catch(function(error) {
+  console.log("log out error");
+});
 
-console.log(firebase.app().name);  // "[DEFAULT]"
-console.log(otherProject.name);    // "otherProject"
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var email = user.email;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+    console.log("user signed out");
+  }
+});
 
-// Use the shorthand notation to access the default project's Firebase services
-var defaultStorage = firebase.storage();
-var defaultFirestore = firebase.firestore();
-
-// Use the otherProject variable to access the second project's Firebase services
-var otherStorage = otherProject.storage();
-var otherFirestore = otherProject.firestore();
-
-  
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+  console.log('User Logged Out!');
+}).catch(function(error) {
+  // An error happened.
+  console.log(error);
+});
