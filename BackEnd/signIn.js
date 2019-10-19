7ee437ccc1a1731d2eeb90edffa809538da4afc2
Busher <bridibusher@gmail.com>
2019-10-18 22:35:58 -0700

    Added driver functions

--- BackEnd/signIn.js
+++ BackEnd/signIn.js
@@ -1,67 +1,35 @@
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-  
-
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      {
-        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        requireDisplayName: True
-      }
-    ]
-  });
-
-
-
-  
-  var uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
-      uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
-        document.getElementById('loader').style.display = 'none';
-      }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.PhoneAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url.
-    privacyPolicyUrl: '<your-privacy-policy-url>'
-  };
-  
-  // Initialize Firebase with a default Firebase project
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "",
+  authDomain: "ucmroboticsfoodrobot.firebaseapp.com",
+  databaseURL: "https://ucmroboticsfoodrobot.firebaseio.com",
+  projectId: "ucmroboticsfoodrobot",
+  storageBucket: "ucmroboticsfoodrobot.appspot.com",
+  messagingSenderId: "503157735593",
+  appId: "1:503157735593:web:0161305286f5636114e9b8",
+  measurementId: "G-1SHQ20SNTN"
+};
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
 
-// Initialize Firebase with a second Firebase project
-var otherProject = firebase.initializeApp(otherProjectFirebaseConfig, "other");
 
-console.log(firebase.app().name);  // "[DEFAULT]"
-console.log(otherProject.name);    // "otherProject"
 
-// Use the shorthand notation to access the default project's Firebase services
-var defaultStorage = firebase.storage();
-var defaultFirestore = firebase.firestore();
+const form = document.querySelector('#signIn-form')
 
-// Use the otherProject variable to access the second project's Firebase services
-var otherStorage = otherProject.storage();
-var otherFirestore = otherProject.firestore();
+//Sign up new users: 
+form.addEventListener('submit', (e) => {
+  var useremail = form.userEmail.value;
+  var userpassword = form.userPassword.value;
+  console.log(useremail)
+  e.preventDefault();
+firebase.auth().createUserWithEmailAndPassword(useremail, userpassword).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+})
+});
 
-  
+
