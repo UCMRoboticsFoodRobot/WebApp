6acdf82d19085f6ab83f673d0811ee97e4f94224
Yash Sharma <ysharma3@ucmerced.edu>
2019-11-02 16:48:33 -0700

    making updateable list for items ordered

--- BackEnd/orderhistory.js
+++ BackEnd/orderhistory.js
@@ -1,11 +1,31 @@
+const currentTable = document.querySelector('#currentOrderTable')
+
 
 //create element and render login
-function renderOrder(doc, id){
+function renderOrder(doc){
 
-    let subtotal = document.createElement('span');
-    let taxes = document.createElement('span');
-    let Total = document.createElement('span');
-    let transport = document.createElement('span');
+    let total = document.createElement('span');
+    let quantity = document.createElement('ul');
+    let item = document.createElement('ul');
+
+
+    let li = document.createElement('li');
+
+    let name = document.createElement('span');
+    let username = document.createElement('span');
+    
+    //sets the data values to variables
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    username.textContent = doc.data().username;
+
+    //adds the data to the li
+      
+      li.appendChild(name);
+      li.appendChild(username);
+
+    //adds the li to our loginlist id
+    ul.appendChild(li);
     
     
   }
