a445ac19bfb995a7a1da34bcb28f97910cc42860
Yash Sharma <msharma@Maheshs-MacBook-Pro.local>
2019-11-26 17:03:47 -0800

    working on getting data from a hashmap

--- BackEnd/orderhistory.js
+++ BackEnd/orderhistory.js
@@ -1,49 +1,62 @@
-const currentTable = document.querySelector('#currentOrderTable')
+
+const currentTable = document.querySelector('#current-order-table')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA-p1zXRhGmSsA8ePtZO8Qbb3KVaUNh7TY",
+  authDomain: "ucmroboticsfoodrobot.firebaseapp.com",
+  databaseURL: "https://ucmroboticsfoodrobot.firebaseio.com",
+  projectId: "ucmroboticsfoodrobot",
+  storageBucket: "ucmroboticsfoodrobot.appspot.com",
+  messagingSenderId: "503157735593",
+  appId: "1:503157735593:web:0161305286f5636114e9b8",
+  measurementId: "G-1SHQ20SNTN"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
 
 
 //create element and render login
 function renderOrder(doc){
 
-    let total = document.createElement('span');
-    let quantity = document.createElement('ul');
-    let item = document.createElement('ul');
-
+    let tr = document.createElement('tr')
+    let price = document.createElement('td');
+    let quantity = document.createElement('td');
+    let item = document.createElement('td');
 
     let li = document.createElement('li');
 
-    let name = document.createElement('span');
-    let username = document.createElement('span');
-    
     //sets the data values to variables
-    li.setAttribute('data-id', doc.id);
+    tr.setAttribute('data-id', doc.id);
     
-    name.textContent = doc.data().name;
-    username.textContent = doc.data().username;
 
-
-    doc.data().Items.forEach(li.appendChild(key));
-    //adds the data to the li
-    for(doc.data().Items.length){
-      li.appendChild(name);
-      li.appendChild(username);
-    }
     //adds the li to our loginlist id
-    ul.appendChild(li);
+    var map;
+    var entry
+    map = doc.data().Items;
+
+    map.forEach(maps => {
+      item = entry.String;
+      quantity = entry.Array[0];
+      price = ent[1];
+      tr.appendChild(item);
+      tr.appendChild(quantity);
+      tr.appendChild(price);
+      currentTable.appendChild(tr);
+    })
+      
+    
     
     
   }
   
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      db.collection('OrderSummary').where(userUID, "==", user.uid).get().then((snapshot) => {
+      db.collection('OrderSummary').get().then((snapshot) => {
         snapshot.docs.forEach(doc => {
           renderOrder(doc);
   
         }) 
       })
-    } else {
-      alert("Please sign in")
-    }
-  });
   
   
