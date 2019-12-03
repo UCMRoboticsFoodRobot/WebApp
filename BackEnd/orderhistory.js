25a8b2716072d1100c5ac73e2652bb9174556065
Yash Sharma <msharma@Maheshs-MacBook-Pro.local>
2019-12-02 21:38:02 -0800

    got previous order history table working

--- BackEnd/orderhistory.js
+++ BackEnd/orderhistory.js
@@ -1,5 +1,6 @@
 
 const currentTable = document.getElementById("current-order-table")
+const previousTable = document.getElementById("previous-order-table")
 
 
 
@@ -46,6 +47,38 @@ function renderOrder(doc){
           tr.append(quantity);
           tr.append(price);
           currentTable.appendChild(tr);
+
+        }
+    }
+    {
+      myMap = doc.data().Items;
+      
+      //loops through hashmap and pushes values to table
+        for (let i of Object.keys(myMap)) {
+          let tr = document.createElement('tr')
+          let price = document.createElement('td');
+          let quantity = document.createElement('td');
+          let item = document.createElement('td');
+          let date = document.createElement('td');
+          let orderID = document.createElement('td');
+          let dateObj = doc.data().time.toDate()
+          
+
+
+          item.innerHTML = i
+          arr = myMap[i]
+          price.innerHTML = "$ " + arr[0].toFixed(2)
+          quantity.innerHTML = arr[1]
+          date.innerHTML = (dateObj.getMonth()+1).toString() + "/" + dateObj.getDate().toString()  + "/" +  dateObj.getFullYear().toString()
+          orderID.innerHTML = doc.data().OrderId
+
+          tr.append(date);
+          tr.append(item);
+          tr.append(quantity);
+          tr.append(price);
+          tr.append(orderID);
+
+          previousTable.appendChild(tr);
 
         }
     }
