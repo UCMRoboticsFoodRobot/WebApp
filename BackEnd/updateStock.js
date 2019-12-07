4b7854594cfd5485b9685a0e1dc03fd92f90bbc4
Yash Sharma <mr.sharmayash@outlook.com>
2019-12-07 01:29:50 -0800

    added backend functionality of update stock

--- BackEnd/updateStock.js
+++ BackEnd/updateStock.js
@@ -1,22 +1,3 @@
-const form = document.querySelector('#itemUpdate-form')
-
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  db.collection('Item').doc(form.itemName.value).set({
-    itemName: form.itemName.value,
-    itemPrice: form.itemPrice.value,
-    itemStock: form.itemsInStock.value,
-})
-  form.itemName.value = '';
-  form.itemPrice.value = '';
-  form.itemsInStock.value = '';
-  form.isInStock.value = '';
-
-})
-
-const itemList = document.getElementById('item-table')
-
 var firebaseConfig = {
   apiKey: "p1zXRhGmSsA8ePtZO8Qbb3KVaUNh7TY",
   authDomain: "ucmroboticsfoodrobot.firebaseapp.com",
@@ -34,12 +15,13 @@ const db = firebase.firestore();
 db.settings({ timestampsInSnapshots: true });
 
 
-//create element and render login
+const itemList = document.getElementById('item-table')
 function renderItem(doc){
 
   let tr = document.createElement('tr');
   let itemname = document.createElement('td');
   let itemprice = document.createElement('td');
+  let itemquantity = document.createElement('td');
   
   //sets the data values to variables
   
@@ -47,16 +29,39 @@ function renderItem(doc){
 
   itemname.innerHTML = doc.data().name;
   itemprice.innerHTML = doc.data().price;
+  itemquantity.innerHTML = doc.data().quantity;
 
 
   //adds the data to the li
   tr.append(itemname);
+  tr.append(itemquantity);
   tr.append(itemprice);
 
   itemList.appendChild(tr);
 
 
 }
+
+itemname = document.getElementById("itemName")
+itemprice = document.getElementById("itemPrice")
+itemStock = document.getElementById("itemsInStock")
+
+function updateStock(){
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('Items').doc().set({
+      name: itemname.innerHTML,
+      price: itemprice.innerHTML,
+      quantity: itemStock.innerHTML,
+  })
+    itemname.innerHTML = '';
+    itemprice.innerHTML = '';
+    itemStock.innerHTML = '';
+
+  })
+}
+
 
 //getting data
 db.collection('Items').get().then((snapshot) => {
