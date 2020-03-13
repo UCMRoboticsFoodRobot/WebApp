01e8a01208c36119bb548d1d7d776ec3374dc45e
Paulo Alberto Sousa <p.sousa612@gmail.com>
2020-03-13 02:31:08 -0700

    Redo the signup and signin pages w/t Bootstrap 4
    Should probably change the gradient backgrounds. Also petition to change signIn -> Login for clarity and visibility. Also moving to a single style.css page for the pages due to Bootstrap handling most of our styling needs. The css page can be used for smaller things like colors that Bootstrap doesn't allow us to change in our current setup.

--- BackEnd/signIn.js
+++ BackEnd/signIn.js
@@ -1,19 +1,5 @@
 
- // Your web app's Firebase configuration
- var firebaseConfig = {
-  apiKey: "AIzaSyA-p1zXRhGmSsA8ePtZO8Qbb3KVaUNh7TY",
-  authDomain: "ucmroboticsfoodrobot.firebaseapp.com",
-  databaseURL: "https://ucmroboticsfoodrobot.firebaseio.com",
-  projectId: "ucmroboticsfoodrobot",
-  storageBucket: "ucmroboticsfoodrobot.appspot.com",
-  messagingSenderId: "503157735593",
-  appId: "1:503157735593:web:0161305286f5636114e9b8",
-  measurementId: "G-1SHQ20SNTN"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-db.settings({ timestampsInSnapshots: true });
 
 //Sign in existing users:
 
@@ -22,32 +8,28 @@ const SignInform = document.querySelector('#signIn-form')
 SignInform.addEventListener('submit', (e) => {
   e.preventDefault();
 var useremailSignIn = SignInform.userEmailSignIn.value;
-var userpasswordSignIn = SignInform.userPasswordSignIn.value;//User 
+var userpasswordSignIn = SignInform.userPasswordSignIn.value;//User
 firebase.auth().signInWithEmailAndPassword(useremailSignIn, userpasswordSignIn).catch(function(error) {
-  
+
   var errorCode = error.code;
   var errorMessage = error.message;
   var len = error.message.length;
   if(len != 0){
     alert("wrong credentials / user doesnt exist");
   }
-
-  // ...
 })
 });
 
-//Check if user is signed in: 
+//Check if user is signed in:
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
    console.log('Signed In');
-   location.replace("../../Frontend/MainPage/main.html") //This need to be fixed to the actual https url later on.. 
+   location.replace("../../Frontend/MainPage/main.html") //This need to be fixed to the actual https url later on..
     // ...
-  } 
+  }
   else{
     console.log("signed out");
   }
 });
-
-
--- BackEnd/signUp.js
+++ BackEnd/signUp.js
@@ -1,21 +1,6 @@
 
- // Your web app's Firebase configuration
- var firebaseConfig = {
-  apiKey: "AIzaSyA-p1zXRhGmSsA8ePtZO8Qbb3KVaUNh7TY",
-  authDomain: "ucmroboticsfoodrobot.firebaseapp.com",
-  databaseURL: "https://ucmroboticsfoodrobot.firebaseio.com",
-  projectId: "ucmroboticsfoodrobot",
-  storageBucket: "ucmroboticsfoodrobot.appspot.com",
-  messagingSenderId: "503157735593",
-  appId: "1:503157735593:web:0161305286f5636114e9b8",
-  measurementId: "G-1SHQ20SNTN"
-};
 
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-db.settings({ timestampsInSnapshots: true });
 
 const signUpform = document.querySelector('#signUp-form')
 
@@ -23,7 +8,7 @@ signUpform.addEventListener('submit', (e) => {
 e.preventDefault();
 
 var useremailSignUp = signUpform.userEmailSignUp.value;
-var userpasswordSignUp = signUpform.userPasswordSignUp.value; //User 
+var userpasswordSignUp = signUpform.userPasswordSignUp.value; //User
 var userpasswordConfirmSignUp = signUpform.userPasswordConfirmSignUp.value;
 
 
@@ -32,7 +17,7 @@ if(userpasswordSignUp === userpasswordConfirmSignUp){
 firebase.auth().createUserWithEmailAndPassword(useremailSignUp, userpasswordSignUp)
 .then(()=>{
     console.log('Signup successful.');
-    
+
    })
 .catch((error)=> {
   alert(error.message)
@@ -45,6 +30,3 @@ else{
   console.log("passwords didnt match")
 }
 });
-
-
-
